6ff6ebaae71f0f42421dc6fc0bd068e1040a3b1a
TRAHIM86 <trahim1986@gmail.com>
2025-04-12 21:58:12 +0300

    feat: completed task 2

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -64,8 +64,14 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.any(promises)
+    .then((fastResult) => {
+      return fastResult;
+    })
+    .catch((error) => {
+      throw error;
+    });
 }
 
 /**
@@ -87,8 +93,14 @@ function getFirstResolvedPromiseResult(/* promises */) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return Promise.race(promises)
+    .then((fastResult) => {
+      return fastResult;
+    })
+    .catch((err) => {
+      throw err;
+    });
 }
 
 /**
@@ -102,8 +114,14 @@ function getFirstPromiseResult(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises)
+    .then((allPromises) => {
+      return allPromises;
+    })
+    .catch((err) => {
+      throw err;
+    });
 }
 
 /**
